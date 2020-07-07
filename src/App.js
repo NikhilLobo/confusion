7d6667a4e8db311bd06f3ef87116e27c22436727
Nikhil Lobo <nikhillobo007@gmail.com>
2020-07-06 19:09:24 -0500

    Configured React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Navbar,NavbarBrand} from 'reactstrap'
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component{
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dark color="primary">
+      <div className="container">
+        <NavbarBrand href="/">Confusion Nikhil</NavbarBrand>
+      </div>
+      </Navbar>
+
+
     </div>
   );
+}
 }
 
 export default App;
